abc58bd23bbe8fd197a1b0086eba287cdebea9e5
James <super890421@gmail.com>
2024-08-04 17:56:02 +0800

    Time: 1 ms (80.62%), Space: 45.5 MB (61.82%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -2,53 +2,38 @@ class Solution {
     public void setZeroes(int[][] matrix) {
         int row = matrix.length;
         int col = matrix[0].length;
-        int[][] m_up = new int[row][col];
-        int[][] m_down = new int[row][col];
+        boolean row0 = false;
 
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
-                if (matrix[i][j] == 0) {
-                    int tem_i = i;
-                    int tem_j = j;
-                    while (tem_i >= 0) {
-                        m_up[tem_i][j] = 0;
-                        tem_i--;
+        for (int r = 0; r < row; r++) {
+            for (int c = 0; c < col; c++) {
+                if (matrix[r][c] == 0) {
+                    matrix[0][c] = 0;
+                    if (r > 0) {
+                        matrix[r][0] = 0;
+                    } else {
+                        row0 = true;
                     }
-                    while (tem_j >= 0) {
-                        m_up[i][tem_j] = 0;
-                        tem_j--;
-                    }
-                }else{
-                    m_up[i][j] = matrix[i][j];
                 }
             }
         }
 
-        for (int i = row - 1; i >= 0; i--) {
-            for (int j = col - 1; j >= 0; j--) {
-                if (matrix[i][j] == 0) {
-                    int tem_i = i;
-                    int tem_j = j;
-                    while (tem_i < row) {
-                        m_down[tem_i][j] = 0;
-                        tem_i++;
-                    }
-                    while (tem_j < col) {
-                        m_down[i][tem_j] = 0;
-                        tem_j++;
-                    }
-                }else{
-                    m_down[i][j] = matrix[i][j];
+        for (int r = 1; r < row; r++) {
+            for (int c = 1; c < col; c++) {
+                if (matrix[0][c] == 0 || matrix[r][0] == 0) {
+                    matrix[r][c] = 0;
                 }
             }
         }
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
-                if (m_up[i][j] == 0 || m_down[i][j] == 0) {
-                    matrix[i][j] = 0;
-                }
+
+        if (matrix[0][0] == 0) {
+            for (int r = 1; r < row; r++) {
+                matrix[r][0] = 0;
+            }
+        }
+        if (row0) {
+            for (int c = 0; c < col; c++) {
+                matrix[0][c] = 0;
             }
         }
     }
-
 }
