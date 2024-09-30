ff5f53acb362e742b05b19cacf7bf0fac615b05d
James <super890421@gmail.com>
2024-10-01 00:03:14 +0800

    Time: 0 ms (100%), Space: 45.6 MB (63.57%) - LeetHub

--- 0792-binary-search/0792-binary-search.java
+++ 0792-binary-search/0792-binary-search.java
@@ -1,23 +1,17 @@
 class Solution {
     public int search(int[] nums, int target) {
-        return getNum(nums, 0, nums.length - 1, target);
-    }
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
 
-    // [-1,0,3,5,9,12]
-    private int getNum(int[] nums, int min, int max, int target) {
-        int mid = (max + min) / 2;
-        if (nums[mid] == target)
-            return mid;
-        else if (min + 1 == max && nums[mid + 1] == target) {
-            return mid + 1;
-        } else if (min + 1 == max || min == max) {
-            return -1;
+            if (nums[mid] == target)
+                return mid;
+            else if (target > nums[mid])
+                left = mid + 1;
+            else if (target < nums[mid])
+                right = mid - 1;
         }
-
-        if (target > nums[mid])
-            return getNum(nums, mid, max, target);
-        else
-            return getNum(nums, min, mid, target);
-
+        return -1;
     }
 }
