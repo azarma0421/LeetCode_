4ee5f5de61dbe194ee534a332b0c85fc92c8d904
James <super890421@gmail.com>
2024-10-03 19:46:29 +0800

    Time: 0 ms (100%), Space: 39.7 MB (98.21%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int climbStairs(int n) {
+        if (n == 1)
+            return 1;
+        else if (n == 2)
+            return 2;
+            
+        int[] step = new int[n + 1];
+        step[1] = 1;
+        step[2] = 2;
+
+        for (int i = 3; i <= n; i++) {
+            step[i] = step[i - 1] + step[i - 2];
+        }
+        return step[n];
+    }
+}
