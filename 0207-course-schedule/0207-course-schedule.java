1a0dc6ada8065dd1d79730213fb37dcdff4959cd
James <super890421@gmail.com>
2024-10-22 22:43:21 +0800

    Time: 2 ms (99.96%), Space: 44.7 MB (77.14%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,39 +1,46 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        ArrayList[] graph = new ArrayList[numCourses];
-        Queue queue = new LinkedList();
-        int course = 0;
+        int[] visited = new int[numCourses];
+        ArrayList<Integer>[] adj = new ArrayList[numCourses];
 
-        for (int i = 0; i < numCourses; i++) {
-            graph[i] = new ArrayList<Integer>();
-        }
-        for (int i = 0; i < prerequisites.length; i++) {
-            graph[prerequisites[i][0]].add(prerequisites[i][1]);
+        for(int i=0;i<numCourses;i++){
+            adj[i] = new ArrayList<>();
         }
 
-        for (int i = 0; i < graph.length; i++) {
-            if (graph[i].size() == 0) {
-                course++;
-                queue.add(i);
+        for(int[] pre : prerequisites){
+            adj[pre[0]].add(pre[1]);
+        }
+
+
+        for(int i=0;i<numCourses;i++){
+            if(!dfs(i,adj,visited)){
+                return false;
             }
         }
+        return true;
+    }
 
-        while (queue.size() != 0) {
-            int finish = (int) queue.poll();
-            for (int i = 0; i < graph.length; i++) {
-                if (graph[i].contains(finish)) {
-                    graph[i].remove(Integer.valueOf(finish));
-                    if (graph[i].size() == 0) {
-                        course++;
-                        queue.add(i);
-                    }
-                }
-            }
+    public boolean dfs(int node, ArrayList<Integer>[] adj,int[] visited){
+
+        // already been here
+        if(visited[node] == 1){
+            return false;
         }
 
-        if (course == numCourses)
+        // already finish
+        if(visited[node] == 2){
             return true;
+        }
 
-        return false;
+        visited[node] = 1;
+        for(int j : adj[node]){
+            if(!dfs(j,adj,visited)){
+                return false;
+            }
+        }
+
+        visited[node] = 2;
+        return true;
+
     }
 }
