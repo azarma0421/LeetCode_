8abb09c7c146afe7c8e6cd439bafe0798c144be4
James <super890421@gmail.com>
2024-10-24 22:43:15 +0800

    Time: 5 ms (77.51%), Space: 44.5 MB (86.32%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,36 +1,37 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
         ArrayList[] graph = new ArrayList[numCourses];
-        int[] degree = new int[numCourses];
-        Queue queue = new LinkedList();
-        int count = 0;
+        int[] corse = new int[numCourses];
+        Queue queue = new LinkedList<>();
+        int finish = 0;
 
         for (int i = 0; i < numCourses; i++)
             graph[i] = new ArrayList();
 
         for (int i = 0; i < prerequisites.length; i++) {
             graph[prerequisites[i][1]].add(prerequisites[i][0]);
-            degree[prerequisites[i][0]]++;
+            corse[prerequisites[i][0]]++;
         }
 
-        for (int i = 0; i < degree.length; i++) {
-            if (degree[i] == 0) {
-                count++;
+        for (int i = 0; i < corse.length; i++) {
+            if (corse[i] == 0) {
+                finish++;
                 queue.add(i);
             }
         }
 
         while (queue.size() != 0) {
-            int currentCourse = (int) queue.poll();
-            for (int i = 0; i < graph[currentCourse].size(); i++) {
-                int dependentCourse = (int) graph[currentCourse].get(i);
-                degree[dependentCourse]--;
-                if (degree[dependentCourse] == 0) {
-                    count++;
-                    queue.add(dependentCourse);
+            int current = (int) queue.poll();
+            for (int i = 0; i < graph[current].size(); i++) {
+                int point = (int) graph[current].get(i);
+                corse[point]--;
+                if (corse[point] == 0) {
+                    finish++;
+                    queue.add(point);
                 }
             }
         }
-        return count == numCourses;
+
+        return finish == numCourses;
     }
 }
