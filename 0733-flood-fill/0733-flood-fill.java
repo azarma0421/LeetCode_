c75639b62ae160bce72dd9fb3a067c09cc6c5699
James <super890421@gmail.com>
2024-10-27 23:52:24 +0800

    Time: 1 ms (44.82%), Space: 45 MB (20.91%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,5 +1,6 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        int originalColor = image[sr][sc];
         if (image[sr][sc] == color)
             return image;
 
@@ -7,37 +8,29 @@ class Solution {
         int y = image[0].length;
         int[][] visited = new int[x][y];
 
+        int[][] directions = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
+
         Queue<int[]> queue = new LinkedList();
-        dfs(image, visited, queue, sr, sc, image[sr][sc]);
+        queue.add(new int[] { sr, sc });
 
         while (queue.size() != 0) {
-            int[] site = (int[]) queue.poll();
-            image[site[0]][site[1]] = color;
+            int[] current = (int[]) queue.poll();
+            int r = current[0];
+            int c = current[1];
+
+            image[r][c] = color;
+
+            for (int[] dir : directions) {
+                int newRow = r + dir[0];
+                int newCol = c + dir[1];
+
+                if (newRow >= 0 && newRow < x && newCol >= 0 && newCol < y &&
+                        image[newRow][newCol] == originalColor) {
+                    queue.add(new int[] { newRow, newCol });
+                }
+            }
         }
 
         return image;
     }
-
-    private void dfs(int[][] image, int[][] visited, Queue queue, int sr, int sc, int color) {
-        int x = image.length;
-        int y = image[0].length;
-
-        if (x <= sr || y <= sc || sr < 0 || sc < 0 || visited[sr][sc] != 0)
-            return;
-
-        if (color != image[sr][sc]) {
-            visited[sr][sc] = 2;
-            return;
-        }
-
-        queue.add(new int[] { sr, sc });
-        visited[sr][sc] = 1;
-
-        dfs(image, visited, queue, sr - 1, sc, color);
-        dfs(image, visited, queue, sr + 1, sc, color);
-        dfs(image, visited, queue, sr, sc - 1, color);
-        dfs(image, visited, queue, sr, sc + 1, color);
-
-    }
-
 }
