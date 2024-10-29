3bf5f19fc40f5430dc6ab2970b552e94e1361e10
James <super890421@gmail.com>
2024-10-29 23:08:28 +0800

    Time: 4 ms (49.73%), Space: 49.4 MB (62.01%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,36 +1,26 @@
 class Solution {
+    char[][] g;
+
     public int numIslands(char[][] grid) {
-        int x = grid.length;
-        int y = grid[0].length;
-
         int islands = 0;
-        int[][] visited = new int[x][y];
-        Queue queue = new LinkedList();
-
-        int[][] direction = new int[][] { { 0, 1 }, { 1, 0 }, { -1, 0 }, { 0, -1 } };
-
-        for (int i = 0; i < x; i++) {
-            for (int j = 0; j < y; j++) {
-                if (grid[i][j] == '1' && visited[i][j] != 1) {
-                    islands++;
-                    queue.add(new int[] { i, j });
-                    while (queue.size() != 0) {
-                        int[] current = (int[]) queue.poll();
-                        for (int[] dir : direction) {
-                            int newX = current[0] + dir[0];
-                            int newY = current[1] + dir[1];
-                            if (newX >= 0 && newX < x && newY >= 0 && newY < y
-                                    && visited[newX][newY] != 1 && grid[newX][newY] == '1') {
-                                queue.add(new int[] { newX, newY });
-                                visited[newX][newY] = 1;
-                            }
-                        }
-                    }
-                }
-                visited[i][j] = 1;
-            }
-        }
-
+        g = grid;
+        for (int i = 0; i < g.length; i++)
+            for (int j = 0; j < g[0].length; j++)
+                islands += sink(i, j);
         return islands;
+    }
+
+    private int sink(int i, int j) {
+        if (i < 0 || i >= g.length || j < 0 || j >= g[0].length || g[i][j] == '0')
+            return 0;
+
+        g[i][j] = '0';
+
+        sink(i + 1, j);
+        sink(i - 1, j);
+        sink(i, j + 1);
+        sink(i, j - 1);
+
+        return 1;
     }
 }
