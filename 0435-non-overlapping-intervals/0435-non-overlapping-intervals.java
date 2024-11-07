32df2873d5f8cc027f6919b9aeeea701bec87258
James <super890421@gmail.com>
2024-11-07 22:35:05 +0800

    Time: 48 ms (60.32%), Space: 74.1 MB (16.03%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
@@ -3,19 +3,16 @@ class Solution {
         if (intervals.length == 1)
             return 0;
 
-        Arrays.sort(intervals, (a, b) -> Integer.compare(a[1], b[1]));
         int pre = 0;
         int join = 1;
-        int n = intervals.length;
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[1], b[1]));
 
-        for (int i = 1; i < n; i++) {
+        for (int i = 1; i < intervals.length; i++) {
             if (intervals[i][0] >= intervals[pre][1]) {
                 pre = i;
                 join++;
             }
         }
-
-        return n - join;
-
+        return intervals.length - join;
     }
 }
