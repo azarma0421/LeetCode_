a2ca29daf3f35261bb974fba7ba4ff579c04917e
James <super890421@gmail.com>
2024-12-15 22:11:46 +0800

    Time: 3 ms (87.98%), Space: 45.3 MB (40.82%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,35 +1,30 @@
 class Solution {
     public String minWindow(String s, String t) {
-        if (s == null || t == null || s.length() == 0
-                || t.length() == 0 || s.length() < t.length()) {
-            return new String();
-        }
         int[] map = new int[128];
-        int len = Integer.MAX_VALUE;
-        int start = 0, end = 0, startIndex = 0;
         int count = t.length();
+        int minlen = Integer.MAX_VALUE;
+        int start = 0;
+        int end = 0;
+        int startIndex = 0;
+
+        char[] chS = s.toCharArray();
 
         for (char c : t.toCharArray()) {
             map[c]++;
         }
 
-        char[] chS = s.toCharArray();
-
         while (end < s.length()) {
-            if (map[chS[end++]]-- > 0) {
+            if (map[chS[end++]]-- > 0)
                 count--;
-            }
             while (count == 0) {
-                if (len > end - start) {
-                    len = end - start;
+                if (minlen > end - start) {
                     startIndex = start;
+                    minlen = end - start;
                 }
-                if (map[chS[start++]]++ == 0) {
+                if (map[chS[start++]]++ == 0)
                     count++;
-                }
             }
         }
-
-        return len == Integer.MAX_VALUE ? new String() : new String(chS, startIndex, len);
+        return minlen == Integer.MAX_VALUE ? new String() : new String(chS, startIndex, minlen);
     }
 }
