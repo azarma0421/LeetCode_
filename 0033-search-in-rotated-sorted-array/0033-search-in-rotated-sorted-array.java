b043aa3ff9823b846b99ce293f13cc8d197e7c57
James <super890421@gmail.com>
2025-02-20 22:03:43 +0800

    Time: 0 ms (100%), Space: 42.3 MB (28.53%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,24 +1,30 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int l = 0;
-        int r = nums.length - 1;
-        while (l <= r) {
-            int mid = (l + r) / 2;
-            if (nums[mid] == target)
-                return mid;
+        int left = 0;
+        int right = nums.length - 1;
+        return findRotated(nums, target, left, right);
+    }
 
-            if (nums[l] <= nums[mid]) {
-                if (target < nums[mid] && target >= nums[l])
-                    r = mid - 1;
-                else
-                    l = mid + 1;
+    private int findRotated(int[] nums, int target, int left, int right) {
+        int mid = (left + right) / 2;
+
+        if (nums[mid] == target)
+            return mid;
+        if (left >= right)
+            return -1;
+
+        if (nums[mid] > nums[right]) {
+            if (nums[left] <= target && target < nums[mid]) {
+                return findRotated(nums, target, left, mid - 1);
             } else {
-                if (target >= nums[mid] && target <= nums[r])
-                    l = mid + 1;
-                else
-                    r = mid - 1;
+                return findRotated(nums, target, mid + 1, right);
+            }
+        } else {
+            if (nums[mid] < target && target <= nums[right]) {
+                return findRotated(nums, target, mid + 1, right);
+            } else {
+                return findRotated(nums, target, left, mid - 1);
             }
         }
-        return -1;
     }
 }
