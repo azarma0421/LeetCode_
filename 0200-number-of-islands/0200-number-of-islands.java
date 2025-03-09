29df213e174178795fbf0ab420379206b949e5f4
James <super890421@gmail.com>
2025-03-09 22:31:18 +0800

    Time: 3 ms (86.57%), Space: 50 MB (31.33%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,24 +1,26 @@
 class Solution {
     public int numIslands(char[][] grid) {
-        int islands = 0;
+        int island = 0;
         for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
+            for (int j = 0; j < grid[i].length; j++) {
                 if (grid[i][j] == '1') {
-                    islands++;
-                    sink(i, j, grid);
+                    island++;
+                    sink(grid, i, j);
                 }
             }
         }
-        return islands;
+        return island;
     }
 
-    private void sink(int i, int j, char[][] grid) {
-        if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] == '0')
+    private void sink(char[][] grid, int i, int j) {
+        if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length)
+            return;
+        if (grid[i][j] == '0')
             return;
         grid[i][j] = '0';
-        sink(i + 1, j, grid);
-        sink(i - 1, j, grid);
-        sink(i, j + 1, grid);
-        sink(i, j - 1, grid);
+        sink(grid, i + 1, j);
+        sink(grid, i - 1, j);
+        sink(grid, i, j + 1);
+        sink(grid, i, j - 1);
     }
 }
