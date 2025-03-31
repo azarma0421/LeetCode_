42619d130fec4ea13b11308facb60da858a4f283
James <super890421@gmail.com>
2025-03-31 09:46:05 +0800

    Time: 1 ms (31.7%), Space: 44.5 MB (60.04%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
@@ -1,41 +1,38 @@
-
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- * int val;
- * TreeNode left;
- * TreeNode right;
- * TreeNode() {}
- * TreeNode(int val) { this.val = val; }
- * TreeNode(int val, TreeNode left, TreeNode right) {
- * this.val = val;
- * this.left = left;
- * this.right = right;
- * }
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
 class Solution {
     public int kthSmallest(TreeNode root, int k) {
         Stack<TreeNode> stack = new Stack<>();
-        int count = 0;
-
         while (root != null) {
             stack.push(root);
             root = root.left;
         }
-        root = stack.pop();
 
-        while (true) {
-            if (++count == k)
-                return root.val;
-            if (root.right != null) {
-                root = root.right;
-                while (root != null) {
-                    stack.push(root);
-                    root = root.left;
+        while (k > 0) {
+            k--;
+            root = stack.pop();
+            TreeNode temp = root;
+            if (temp.right != null) {
+                temp = temp.right;
+                while (temp != null) {
+                    stack.push(temp);
+                    temp = temp.left;
                 }
             }
-            root = stack.pop();
         }
+        return root.val;
     }
 }
