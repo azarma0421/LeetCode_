e7cfc478b2ba27afefaf3253682af6c0f9c696b0
James <super890421@gmail.com>
2025-07-16 22:22:00 +0800

    Time: 145 ms (52.62%), Space: 41.6 MB (77.24%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -5,14 +5,14 @@ class Solution {
         boolean[][] visited = new boolean[x][y];
         for (int i = 0; i < x; i++) {
             for (int j = 0; j < y; j++) {
-                if (dfs(word, i, j, board, visited, 0))
+                if (backtrack(word, i, j, board, visited, 0))
                     return true;
             }
         }
         return false;
     }
 
-    private boolean dfs(String word, int x, int y, char[][] board, boolean[][] visited, int matched) {
+    private boolean backtrack(String word, int x, int y, char[][] board, boolean[][] visited, int matched) {
         if (word.length() == matched)
             return true;
         if (x < 0 || y < 0 || x >= board.length || y >= board[0].length || visited[x][y])
@@ -20,8 +20,8 @@ class Solution {
         if (word.charAt(matched) == board[x][y]) {
             matched++;
             visited[x][y] = true;
-            if (dfs(word, x + 1, y, board, visited, matched) || dfs(word, x - 1, y, board, visited, matched) ||
-                    dfs(word, x, y + 1, board, visited, matched) || dfs(word, x, y - 1, board, visited, matched)) {
+            if (backtrack(word, x + 1, y, board, visited, matched) || backtrack(word, x - 1, y, board, visited, matched) ||
+                    backtrack(word, x, y + 1, board, visited, matched) || backtrack(word, x, y - 1, board, visited, matched)) {
                 return true;
             }
             matched--;
