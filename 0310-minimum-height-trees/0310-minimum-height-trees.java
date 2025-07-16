b735f0f940ca1a0af2b8efd7c9f85398bb6ee2e7
James <super890421@gmail.com>
2025-07-16 23:27:16 +0800

    Time: 15 ms (89.52%), Space: 56.4 MB (55.36%) - LeetHub

--- 0310-minimum-height-trees/0310-minimum-height-trees.java
+++ 0310-minimum-height-trees/0310-minimum-height-trees.java
@@ -1,25 +1,26 @@
-;
 class Solution {
     public List<Integer> findMinHeightTrees(int n, int[][] edges) {
         if (n == 1) {
             return Collections.singletonList(0);
         }
+
         int[] degree = new int[n];
         List<List<Integer>> adj = new ArrayList<>();
 
-        for (int i = 0; i < n; i++)
+        for (int i = 0; i < n; i++) {
             adj.add(new ArrayList<>());
+        }
 
         for (int[] edge : edges) {
-            adj.get(edge[0]).add(edge[1]);
-            adj.get(edge[1]).add(edge[0]);
             degree[edge[0]]++;
             degree[edge[1]]++;
+            adj.get(edge[0]).add(edge[1]);
+            adj.get(edge[1]).add(edge[0]);
         }
 
         List<Integer> leaves = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            if (adj.get(i).size() == 1)
+        for (int i = 0; i < degree.length; i++) {
+            if (degree[i] == 1)
                 leaves.add(i);
         }
 
