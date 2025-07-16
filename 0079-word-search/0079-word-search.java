ac270ec220754e9334889290fa1d4da220c84757
James <super890421@gmail.com>
2025-07-16 22:18:36 +0800

    Time: 146 ms (50.33%), Space: 41.5 MB (88.8%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -3,38 +3,30 @@ class Solution {
         int x = board.length;
         int y = board[0].length;
         boolean[][] visited = new boolean[x][y];
-
         for (int i = 0; i < x; i++) {
             for (int j = 0; j < y; j++) {
-                if (dfs(board, visited, i, j, word, 0))
+                if (dfs(word, i, j, board, visited, 0))
                     return true;
             }
         }
         return false;
     }
 
-    private boolean dfs(char[][] board, boolean[][] visited, int x, int y, String word, int index) {
-        if (word.length() == index) {
+    private boolean dfs(String word, int x, int y, char[][] board, boolean[][] visited, int matched) {
+        if (word.length() == matched)
             return true;
-        }
-
-        if (x < 0 || y < 0 ||
-                x >= board.length || y >= board[0].length ||
-                visited[x][y] || board[x][y] != word.charAt(index)) {
+        if (x < 0 || y < 0 || x >= board.length || y >= board[0].length || visited[x][y])
             return false;
+        if (word.charAt(matched) == board[x][y]) {
+            matched++;
+            visited[x][y] = true;
+            if (dfs(word, x + 1, y, board, visited, matched) || dfs(word, x - 1, y, board, visited, matched) ||
+                    dfs(word, x, y + 1, board, visited, matched) || dfs(word, x, y - 1, board, visited, matched)) {
+                return true;
+            }
+            matched--;
+            visited[x][y] = false;
         }
-
-        index++;
-        visited[x][y] = true;
-
-        if (dfs(board, visited, x + 1, y, word, index) ||
-                dfs(board, visited, x - 1, y, word, index) ||
-                dfs(board, visited, x, y + 1, word, index) ||
-                dfs(board, visited, x, y - 1, word, index)) {
-            return true;
-        }
-
-        visited[x][y] = false;
         return false;
     }
 }
